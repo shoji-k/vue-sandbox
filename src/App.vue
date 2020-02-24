f205aca159fb356770fc3bb3b22b07193284bac4
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-02-24 16:08:36 +0900

    vue add vuetify and setting

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="headline">
+        <v-btn text to="/">
+          Vue sandbox
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text to="/about">
+        About
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
