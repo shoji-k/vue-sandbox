e6cee01f836b1ef63a9624b10b74e3093902d27a
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-02-24 16:36:37 +0900

    use router name

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="headline">
-        <v-btn text to="/">
+        <v-btn text :to="{ name: 'home' }">
           Vue sandbox
         </v-btn>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn text to="/about">
+      <v-btn text :to="{ name: 'about' }">
         About
       </v-btn>
     </v-app-bar>
